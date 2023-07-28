e8343a457afcb9a42e926ca52d775be8b741ae2b
lsy969999 <lsy969999@gmail.com>
2023-07-29 01:15:10 +0900

    helloworld

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
+  const dynamicData = await fetch('http://localhost:3001/helloworld', {cache: 'no-store'})
+  const d = await dynamicData.json()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Hellow World</h1>
+        <h1>Hellow World {d.lsy}</h1>
     </main>
   )
 }

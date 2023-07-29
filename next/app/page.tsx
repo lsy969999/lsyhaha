ad702a01f9c7d9dbfd3c4dad25cd50e41e7d8499
lsy969999 <lsy969999@gmail.com>
2023-07-29 17:45:18 +0900

    next test sample login page

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,11 +1,35 @@
+
 import Image from 'next/image'
+import Reload from './component/reload'
+
+type param = {hello: string, lsy: string}
 
 export default async function Home() {
   const dynamicData = await fetch('http://localhost:3001/helloworld', {cache: 'no-store'})
   const d = await dynamicData.json()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Hellow World {d.lsy}</h1>
+    <main className="">
+        <h1>Hellow World{d.lsy}</h1>
+        <section>
+          <div>
+            <button>email login</button>
+          </div>
+          <div>
+            <button>google login</button>
+          </div>
+          <div>
+            <button>kakao login</button>
+          </div>
+          <div>
+            <Reload></Reload>
+          </div>
+        </section>
     </main>
   )
 }
+
+// export async function generateStaticParams(){
+//   const dynamicData = await fetch('http://localhost:3001/helloworld', {cache: 'no-store'})
+//   const d = await dynamicData.json()
+//   return {hello: 'test', lsy: 'haha'}
+// }

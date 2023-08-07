15f9e44abf91f44fa862ce5d39b002ec55991008
lsy969999 <lsy969999@gmail.com>
2023-08-07 23:58:05 +0900

    go

--- next/deploy.sh
+++ next/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-REPOSITORY=/home/ubuntu/deploy # 배포된 프로젝트 경로.
+REPOSITORY=/home/ubuntu/deploy/next # 배포된 프로젝트 경로.
 
 cd $REPOSITORY # 이 경로로 이동해서 밑에 명령어들을 차례로 실행.
 
 sudo npm install # 의존성 파일 설치.
 
-sudo npm run build
+# sudo npm run build
 
-pm2 start npm --name "lsyhahanext" -- start
+sudo pm2 start npm --name "lsyhahanext" -- start
 
 sudo npx pm2 reload all # 프로젝트에서 변경된 내용을 반영하기 위해 pm2를 reload.
